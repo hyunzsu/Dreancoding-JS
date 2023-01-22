19774216fde1d0716c94bf5d5121b80ba78e282e
hyunzsu <kkm5169@naver.com>
2023-01-23 05:00:07 +0900

    [수정] 5-5. 생성자 함수 주석 수정

--- 5.object/5.create.js
+++ 5.object/5.create.js
@@ -1,19 +1,21 @@
-// const apple = {
-//   name: 'apple',
-//   display: function () {
-//     console.log(`${this.name}: 🍎`); // 객체안에서 자신의 이름을 접근할 때 this
-//   }
-// }
+/*
+const apple = {
+  name: 'apple',
+  display: function () {
+    console.log(`${this.name}: 🍎`); // 객체안에서 자신의 이름을 접근할 때 this
+  }
+}
 
-// const orange = {
-//   name: 'orange',
-//   display: function () {
-//     console.log(`${this.name}: 🍊`); // 객체안에서 자신의 이름을 접근할 때 this
-//   }
-// }
+const orange = {
+  name: 'orange',
+  display: function () {
+    console.log(`${this.name}: 🍊`); // 객체안에서 자신의 이름을 접근할 때 this
+  }
+}
+*/
 
-// 넘 귀찮음 생성자함수를 이용해보자
-// 한번만 템플릿을 만들면 돼
+// 생성자 함수
+// 객체를 일일히 추가해주기 귀찮 -> 한번만 템플릿 만들면 됨
 function Fruit(name, emoji) {
   this.name = name; // 객체 자기 자신을 가리킴
   this.emoji = emoji;
@@ -26,5 +28,5 @@ function Fruit(name, emoji) {
 const apple = new Fruit('apple', '🍎')
 const orange = new Fruit('orange', '🍊')
 
-console.log(apple);
-console.log(orange);
+console.log(apple); // Fruit { name: 'apple', emoji: '🍎', display: [Function (anonymous)] }
+console.log(orange); // Fruit { name: 'orange', emoji: '🍊', display: [Function (anonymous)] }
